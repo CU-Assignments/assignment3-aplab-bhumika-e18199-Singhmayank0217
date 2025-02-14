a7b24d14cf3241fa5c0f08b4f2c8a73d1122aec2
Mayank Singh <127119074+Singhmayank0217@users.noreply.github.com>
2025-02-14 17:26:34 +0530

    Create rotateRight.java

---
+++ rotateRight.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) return head;
+        
+        ListNode current = head;
+        int length = 1;
+        
+        while (current.next != null) {
+            current = current.next;
+            length++;
+        }
+        
+        current.next = head;
+        k = k % length;
+        int stepsToNewHead = length - k;
+        
+        ListNode newTail = head;
+        for (int i = 0; i < stepsToNewHead - 1; i++) {
+            newTail = newTail.next;
+        }
+        
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+        
+        return newHead;
+    }
+}
